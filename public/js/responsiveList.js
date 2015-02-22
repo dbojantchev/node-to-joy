945e8bbc663295421385ce0e85dc6c65c7ba0946
Dimitar <dimitar@dimitar-Inspiron-3537.(none)>
2015-02-21 19:52:45 -0800

    Added a service factory

--- public/js/responsiveList.js
+++ public/js/responsiveList.js
@@ -1,14 +1,47 @@
 angular.module('responsiveList', [])
-    .controller('responsiveListController', ['$scope', '$http',
+    .factory("$apiService", ['$http',
+        function($http){
+            return {
+                getDeviceList : function(){
+                    return $http({method: 'GET', url: '/api/getDeviceData?cachebuster=' + (new Date()).getTime()  });
+                },
 
-        function ($scope, $http) {
+                getCreditInfo : function(){
+                    /* to use the real method when ready */
+                    return $http({method: 'GET', url: './data/creditInfo.json'});
+                    //return $http({method: 'GET', url: 'http://ua-rest.902labs.com/creditInfo'});
+                },
+                getAllotmentData : function(){
+                    /* to use the real method when ready */
+                    return $http({method: 'GET', url: './data/allotment.json'});
+                    //return $http({method: 'GET', url: 'http://ua-rest.902labs.com/allotment'});
+                },
+                getBillingAddress : function(){
+                    /* to use the real method when ready */
+                    return $http({method: 'GET', url: './data/billingAddress.json'});
+                    //return $http({method: 'GET', url: 'http://ua-rest.902labs.com/billingAddress'});
+                }
+            };
+        }
+    ])
+    .controller('responsiveListController', ['$scope', '$http', '$apiService',
+
+        function ($scope, $http, $apiService) {
             $scope.deviceList = [];
 
             $scope.textSearch = '';
 
             $scope.loadData = function total() {
-                //$http({method: 'GET', url: './data/DeviceData.json'}).
-               $http({method: 'GET', url: '/api/getDeviceData?cachebuster=' + (new Date).getTime()  }).
+                $apiService.getDeviceList().
+                    success(function(data, status, headers, config) {
+                        $scope.deviceList = data;
+                        console.log('$scope.loadData success');
+                    }).
+                    error(function(data, status, headers, config) {
+                        console.log('Sorry, the device list could not be loaded');
+                    });
+                /*
+               $http({method: 'GET', url: '/api/getDeviceData?cachebuster=' + (new Date()).getTime()  }).
                    success(function(data, status, headers, config) {
                         $scope.deviceList = data;
                         console.log('$scope.loadData success');
@@ -16,6 +49,8 @@ angular.module('responsiveList', [])
                    error(function(data, status, headers, config) {
                         console.log('Sorry, the device list could not be loaded');
                    });
+                   */
+
             };
 
             $scope.loadData();
